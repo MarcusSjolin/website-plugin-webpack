e83bec22b0f48cd91e64ae2dcce9dd68589c0cf3
Marcus Sjolin <marcussjolin@icloud.com>
2015-08-27 00:24:33 +0100

    Added support for watching bundled files if they change

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 var path = require("path")
 
+var renderedBundles = {
+    
+}
+
 exports = module.exports = function(app) {
     app.registerListener ("preGetAsset", function(err, req, res) {
         var route = req.route.query
-        route = route.replace(/\/assets\/[^\/]*/, "")
+        
+        if (req.route.query in renderedBundles) {
+            res.sendFile(renderedBundles[req.route.query])
+        } else {
+            route = route.replace(/\/assets\/[^\/]*/, "")
 
-        var assetPath = app.assetsAvailable[req.params.id].path
+            var assetPath = app.assetsAvailable[req.params.id].path
 
-        var webpack = require("webpack");
+            var webpack = require("webpack");
 
-        var webpackConfig = require(assetPath + "/webpack.config")(assetPath, process.cwd(), route)
-        // returns a Compiler instance
-        var compiler = webpack(webpackConfig, function(err, stats) {
-            if(err)
-                return console.log(err);
-            var jsonStats = stats.toJson();
-            if(jsonStats.errors.length > 0)
-                return console.log(jsonStats.errors);
-            if(jsonStats.warnings.length > 0)
-                console.warn(jsonStats.warnings);
+            var webpackConfig = require(assetPath + "/webpack.config")(assetPath, process.cwd(), route)
+            // returns a Compiler instance
+            var compiler = webpack(webpackConfig).watch({
 
-            res.sendFile(stats.compilation.outputOptions.path + "/" + stats.compilation.outputOptions.filename)
-        });
+            }, function(err, stats) {
+                if(err)
+                    return console.log(err);
+                var jsonStats = stats.toJson();
+                if(jsonStats.errors.length > 0)
+                    return console.log(jsonStats.errors);
+                if(jsonStats.warnings.length > 0)
+                    console.warn(jsonStats.warnings);
+
+                if ( ! (req.route.query in renderedBundles)) {
+                    renderedBundles[req.route.query] = stats.compilation.outputOptions.path + "/" + stats.compilation.outputOptions.filename
+                    res.sendFile(stats.compilation.outputOptions.path + "/" + stats.compilation.outputOptions.filename)
+                }
+            });
+        }
     })
 }
