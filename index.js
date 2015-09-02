b00d81830c98961fd0448787b6aabe64c0b88333
Marcus Sjolin <marcussjolin@icloud.com>
2015-09-02 01:33:28 +0100

    Adding support for webpack and dev reload for BrowserSync

--- index.js
+++ index.js
@@ -4,28 +4,34 @@ var webpack = require("webpack")
 
 module.exports = function(app) {
     for (var i in app.plugins) {
-        addPlugin(null, app.plugins[i])
+        addPlugin(app)(null, app.plugins[i])
     }
 
-    app.registerListener ("preAddPlugin", addPlugin)
+    app.registerListener ("preAddPlugin", addPlugin(app))
 }
 
-function addPlugin(err, plugin) {
-    if (! fs.existsSync (plugin.path + "/webpack.config.js")) {
-        return
-    }
+function addPlugin(app) {
+    return function (err, plugin) {
+        if (! fs.existsSync (plugin.path + "/webpack.config.js")) {
+            return
+        }
 
-    var webpackConfig = require(plugin.path + "/webpack.config")(plugin.path, process.cwd())
-    // returns a Compiler instance
-    var compiler = webpack(webpackConfig).watch({
-        
-    },function(err, stats) {
-        if(err)
-            return console.log(err);
-        var jsonStats = stats.toJson();
-        if(jsonStats.errors.length > 0)
-            return console.log(jsonStats.errors);
-        if(jsonStats.warnings.length > 0)
-            console.warn(jsonStats.warnings);
-    });
+        var webpackConfig = require(plugin.path + "/webpack.config")(plugin.path, process.cwd())
+        // returns a Compiler instance
+        var compiler = webpack(webpackConfig).watch({
+
+        },function(err, stats) {
+            if(err)
+                return app.log('error', err);
+            var jsonStats = stats.toJson();
+            if(jsonStats.errors.length > 0)
+                return app.log('error', jsonStats.errors);
+            if(jsonStats.warnings.length > 0)
+                app.log('warn', jsonStats.warnings);
+
+            if (app.isDev()) {
+                app.devReload()
+            }
+        });
+    }
 }
